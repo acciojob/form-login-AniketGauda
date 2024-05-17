466e55a4c7cc1cbc8037143aec8c4b806d4e504b
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-17 11:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getFormvalue() {
+function myFunction() {
     //Write your code here
 	let fn = document.querySelector("input[name=fname]");
     let ln = document.querySelector("inut[name=lname]");
